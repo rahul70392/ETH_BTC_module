0e3145ea9c17d99f3652f469ed644bb2a121051c
manjesh singh <majeshsingh127@gmail.com>
2020-02-28 12:29:07 +0530

    BTC withdrawal done

--- withdrawal/routes/index.js
+++ withdrawal/routes/index.js
@@ -1,11 +1,8 @@
 import express from "express";
 let router = express.Router();
-import {
-  transaction
-} from "./controllers/btcWithdrawal";
 /* GET home page. */
 router.get('/', (req, res) => {
   res.send("hii manjesh");
 });
-router.post('/trx', transaction);
+
 module.exports = router;
