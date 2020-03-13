f655f8c708f17487503d60999aeb322363adc85f
manjesh singh <majeshsingh127@gmail.com>
2020-03-13 16:16:09 +0530

    update bitcoin testnet to mainnet

--- withdrawal/config/index.js
+++ withdrawal/config/index.js
@@ -1,7 +1,7 @@
 const config = {
     development: {
         db_uri: `mongodb://localhost:27017/BxlendDb`,
-        btc_provider: `http://${process.env.BTC_RPC_USER}:${process.env.BTC_RPC_PASSWORD}@127.0.0.1:18332/`,
+        btc_provider: `http://${process.env.BTC_RPC_USER}:${process.env.BTC_RPC_PASSWORD}@127.0.0.1:8332/`,
         eth_provider: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
         queue_uri: "amqp://pvatqfwu:q2IArlfzojvCwH4bbygXNe8Q5UugpK6W@mustang.rmq.cloudamqp.com/pvatqfwu"
     },
